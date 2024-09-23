54d9fd23e85eb9ee81a201da2912bfc52023c5ae
SamIslam2000 <zmnobab1999@gmail.com>
2024-09-23 19:48:45 +0600

    Donation and history button coller changes

--- JS/utilities.js
+++ JS/utilities.js
@@ -12,12 +12,13 @@ function getTextFieldValueById(id){
 function showSectionById(id){
     document.getElementById('donation-collect').classList.add('hidden');
     document.getElementById('transaction-history').classList.add('hidden');
+
     document.getElementById(id).classList.remove('hidden');
 }
 
-// History
-function showDateTime() {
-    const now = new Date();
-    const formattedDateTime = now.toLocaleString(); // Formats the date and time according to the user's locale
-    document.getElementById('dateTime').innerHTML = `<strong>Date & Time:</strong> ${formattedDateTime}`;
-  }
+// // History
+// function showDateTime() {
+//     const now = new Date();
+//     const formattedDateTime = now.toLocaleString(); // Formats the date and time according to the user's locale
+//     document.getElementById('dateTime').innerHTML = `<strong>Date & Time:</strong> ${formattedDateTime}`;
+//   }
