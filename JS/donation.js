8e60cb6e1d89c0eaf4f907cec2f7964008711820
SamIslam2000 <zmnobab1999@gmail.com>
2024-09-24 23:44:44 +0600

    Border, margin, padding and basic changes in few section

--- JS/donation.js
+++ JS/donation.js
@@ -5,7 +5,8 @@ document.getElementById('noakhali-donate-btn').addEventListener('click',function
     const noakhaliReceived = noakhaliInputMoney + noakhaliRcvBalance;
     console.log(noakhaliReceived,noakhaliRcvBalance,myBalance);
     if(noakhaliInputMoney<=0 || isNaN(noakhaliInputMoney)===true){
-        alert('invalid');
+        alert('Invalid Input. Please, try again!');
+        document.getElementById('noakhali-donate-money').value = '';
     }
     else{
         if(myBalance >= noakhaliInputMoney){
@@ -13,15 +14,17 @@ document.getElementById('noakhali-donate-btn').addEventListener('click',function
             document.getElementById('my-balance').innerText=newMyBalance;
             document.getElementById('noakhali-rcv-balance').innerText=noakhaliReceived;
             const p = document.createElement('p');
-            p.innerHTML=`<h1 class="text-3xl font-semibold">${noakhaliInputMoney} TK. is Donate for Flood at Noakhali, Bangladesh</h1>
-            <p class="text-xl">Date: ${todayDate}</p>
-            `;
+            p.innerHTML=`<div class="border rounded-lg mx-16 my-2 p-6">
+                <h1 class="text-3xl font-semibold">${noakhaliInputMoney} TK. is Donate for Flood at Noakhali, Bangladesh</h1>
+                        <p class="text-xl">Date: ${todayDate}</p>
+            </div>`;
             document.getElementById('transaction-history').appendChild(p);
             modal.showModal();
             document.getElementById('noakhali-donate-money').value = '';
                 }
         else{
-            alert('not enough money')
+            alert("You don't have enough money!");
+            document.getElementById('noakhali-donate-money').value = '';
         }
     }
 }); 
@@ -32,7 +35,8 @@ document.getElementById('feni-donate-btn').addEventListener('click',function(){
     const feniReceived = feniInputMoney + feniRcvBalance;
     console.log(feniReceived,feniRcvBalance,myBalance);
     if(feniInputMoney<=0 || isNaN(feniInputMoney)===true){
-        alert('invalid');
+        alert('Invalid Input. Please, try again!');
+        document.getElementById('feni-donate-money').value = '';
     }
     else{
         if(myBalance >= feniInputMoney){
@@ -40,15 +44,17 @@ document.getElementById('feni-donate-btn').addEventListener('click',function(){
             document.getElementById('my-balance').innerText=newMyBalance;
             document.getElementById('feni-rcv-balance').innerText=feniReceived;
             const p = document.createElement('p');
-            p.innerHTML=`<h1 class="text-3xl font-semibold">${feniInputMoney} TK. is Donate for Flood Relief in Feni,Bangladesh</h1>
-            <p class="text-xl">Date: ${todayDate}</p>
-            `;
+            p.innerHTML=`<div class="border rounded-lg mx-16 my-2 p-6">
+                <h1 class="text-3xl font-semibold ">${feniInputMoney} TK. is Donate for Flood Relief in Feni,Bangladesh</h1>
+                <p class="text-xl">Date: ${todayDate}</p>
+            </div>`;
             document.getElementById('transaction-history').appendChild(p);
             modal.showModal();
             document.getElementById('feni-donate-money').value = '';
                 }
         else{
-            alert('not enough money')
+            alert("You don't have enough money!");
+            document.getElementById('feni-donate-money').value = '';
         }
     }
 }); 
@@ -59,7 +65,8 @@ document.getElementById('quota-donate-btn').addEventListener('click',function(){
     const quotaReceived = quotaInputMoney + quotaRcvBalance;
     console.log(quotaReceived,quotaRcvBalance,myBalance);
     if(quotaInputMoney<=0 || isNaN(quotaInputMoney)===true){
-        alert('invalid');
+        alert('Invalid Input. Please, try again!');
+        document.getElementById('quota-donate-money').value = '';
     }
     else{
         if(myBalance >= quotaInputMoney){
@@ -67,15 +74,17 @@ document.getElementById('quota-donate-btn').addEventListener('click',function(){
             document.getElementById('my-balance').innerText=newMyBalance;
             document.getElementById('quota-rcv-balance').innerText=quotaReceived;
             const p = document.createElement('p');
-            p.innerHTML=`<h1 class="text-3xl font-semibold">${quotaInputMoney} TK. is Aid for Injured in the Quota Movement</h1>
-            <p class="text-xl">Date: ${todayDate}</p>
-            `;
+            p.innerHTML=`<div class="border rounded-lg mx-16 my-2 p-6">
+                <h1 class="text-3xl font-semibold">${quotaInputMoney} TK. is Aid for Injured in the Quota Movement</h1>
+                <p class="text-xl">Date: ${todayDate}</p>
+            </div>`;
             document.getElementById('transaction-history').appendChild(p);
             modal.showModal();
             document.getElementById('quota-donate-money').value = '';
         }
         else{
-            alert('not enough money')
+            alert("You don't have enough money!");
+            document.getElementById('quota-donate-money').value = '';
         }
     }
 }); 
--- JS/history.js
+++ JS/history.js
@@ -3,20 +3,13 @@ document.getElementById('donation').addEventListener('click',function(){
     showSectionById('donation-collect');
     
     document.getElementById('donation').style.backgroundColor = '#B4F461';
-    document.getElementById('transaction').style.backgroundColor = 'white';
+    document.getElementById('transaction').style.backgroundColor = '#F9F7F3';
 });
 document.getElementById('transaction').addEventListener('click',function(){
     console.log('history button clicked');
     showSectionById('transaction-history');
-    document.getElementById('donation').style.backgroundColor = 'white';
+    document.getElementById('donation').style.backgroundColor = '#F9F7F3';
     document.getElementById('transaction').style.backgroundColor = '#B4F461';
 });
 
 const todayDate  = new Date();
-
-// document.getElementById('nav-btn').addEventListener('click',function(){
-//     window.location.href = 'hudai.html';
-// });
-// document.getElementById('nav-home-btn').addEventListener('click',function(){
-//     window.location.href = 'index.html';
-// });
